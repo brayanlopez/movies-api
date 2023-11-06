e7084559b849d245c8a3c798db0ac3d8a9173b52
Brayan Arnulfo Lopez Gonzalez <blopegon@emeal.nttdata.com>
2023-11-06 10:18:06 -0500

    change CRUD to use db and create init files

--- main.py
+++ main.py
@@ -49,8 +49,9 @@ def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
 def get_movies_by_category(category: str = Query(min_length=5, max_length=45)) -> List[Movie]:
-    data = [item for item in movies if item['category'] == category]
-    return JSONResponse(content=data)
+    db = Session()
+    result = db.Query(MovieModel).filter(MovieModel.category == category).all()
+    return JSONResponse(content=jsonable_encoder(result), status_code=200)
 
 
 @app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
@@ -64,22 +65,28 @@ def create_movie(movie: Movie) -> dict:
 
 @app.put('/movies/{id}', tags=['movies'], response_model=dict)
 def update_movie(id: int, movie: Movie) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            item['title'] = movie.title
-            item['overview'] = movie.overview
-            item['year'] = movie.year
-            item['rating'] = movie.rating
-            item['category'] = movie.category
-            return JSONResponse(content={"message": "movie modified succesfully"})
+    db = Session()
+    result = db.Query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(content={"message":"Content not found"}, status_code=404)
+    result.title = movie.title
+    result.overview = movie.overview
+    result.year = movie.year
+    result.rating = movie.rating
+    result.category = movie.category
+    db.commit()
+    return JSONResponse(content={"message": "movie modified succesfully"})
 
 
 @app.delete('/movies/{id}', tags=['movies'], response_model=dict)
 def delete_movie(id: int) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-    return JSONResponse(content={"message": "movie deleted succesfully"})
+    db = Session()
+    result = db.Query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(content={"message":"Content not found"}, status_code=404)
+    db.delete(result)
+    db.commit()
+    return JSONResponse(content={"message": "movie deleted succesfully"}, status_code=200)
 
 
 @app.post("/login", tags=["auth"])
